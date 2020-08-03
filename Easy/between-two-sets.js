a53c12a82cfa819022da558db3b05f4053b9b56b
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2020-08-03 00:03:08 -0300

    update comment between-two-sets

--- Easy/between-two-sets.js
+++ Easy/between-two-sets.js
@@ -1,17 +1,17 @@
-let a = [2,3];
-let b = [2,4];
+let a = [2, 3];
+let b = [2, 4];
 getTotalX(a, b);
+
+// Not counting in vscode + Quokka for some reason, but working on hackerrank
 
 function getTotalX(a, b) {
   let count = 0;
   for (let i = 1; i <= 100; i++) {
     if (a.every((int) => i % int == 0)) {
-      console.log(i)
       if (b.every((int) => int % i == 0)) {
         count++;
       }
     }
   }
-  console.log(count);
   return count;
 }
